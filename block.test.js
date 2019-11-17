a6300b1871774fb1f8bf4d73eb49765d55afee8d
Shedit <pyrt.91@gmail.com>
2019-11-16 19:11:03 -0500

    Third commit, Continued with adding MineBlock and Cryptogenerator function, including test for both of them, all passed.

--- block.test.js
+++ block.test.js
@@ -1,5 +1,8 @@
 const Block = require('./block');
 const { GENESIS_DATA } = require('./config');
+const cryptoHash = require('./crypto-hash');
+
+
 describe('Block', () => {
     const timestamp = 'a-date';
     const lastHash = 'foo-lastHash';
@@ -30,5 +33,33 @@ describe('Block', () => {
             expect(genesisBlock).toEqual(GENESIS_DATA);
         });
     });
+
+    describe('mineBlock()', () => {
+        const lastBlock = Block.genesis();
+        const data = 'mined';
+        const minedBlock = Block.mineBlock({lastBlock, data})
+
+        it('returns a Block instance', () => {
+            expect(minedBlock instanceof Block).toBe(true);
+        });
+
+        it('sets the `lastHash` to be the `hash` of the lastBlock', () => {
+            expect(minedBlock.lastHash).toEqual(lastBlock.hash)
+        })
+
+        it('sets the data', () => {
+            expect(minedBlock.data).toEqual(data);
+        });
+
+        it('sets a timestamp', () => {
+            expect(minedBlock.timestamp).not.toEqual(undefined)
+        });
+
+        it('create a SHA-256 `hash` based on the proper inputs', () => {
+            expect(minedBlock.hash)
+            .toEqual(cryptoHash(minedBlock.timestamp, lastBlock.hash, data));
+        });
+    });
+
 }); 
 
