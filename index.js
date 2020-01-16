a9b6a79f30adf1a66167a04616a6d398628a5ef1
Shedit <pyrt.91@gmail.com>
2020-01-16 17:19:28 -0500

    30, completed the blockchain functionality as a whole

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ const TransactionMiner = require('./app/transaction-miner');
 const isDevelopment = process.env.ENV === 'development';
 
 const DEFAULT_PORT = 3000;
-const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
+const ROOT_NODE_ADDRESS = isDevelopment ? 
+`http://localhost:${DEFAULT_PORT}` :
+'https://intense-wildwood-82940.herokuapp.com';
 const REDIS_URL = isDevelopment ? 
 'redis://127.0.0.1:6379' :
 'redis://h:pdfc772eca829fb913d990d4ddb39e9276ab751c63e612ee4b03359c4bf9cdcc0@ec2-34-205-79-72.compute-1.amazonaws.com:32229';
@@ -39,6 +41,25 @@ app.use(express.static(path.join(__dirname, 'client/dist')));
 app.get('/api/blocks', (req, res) => {
      res.json(blockchain.chain)
 });
+
+app.get('/api/blocks/length', (req, res) => {
+    res.json(blockchain.chain.length);
+});
+
+app.get('/api/blocks/:id', (req, res) => {
+    const { id } = req.params;
+    const { length } = blockchain.chain;
+
+    const blocksReversed = blockchain.chain.slice().reverse();
+
+    let startIndex = (id-1) * 5;
+    let endIndex = id * 5;
+
+    startIndex = startIndex < length ? startIndex : length; 
+    endIndex = endIndex < length ? endIndex : length;
+
+    res.json(blocksReversed.slice(startIndex, endIndex));
+})
 
 app.post('/api/mine', (req, res) =>{
     const { data } = req.body;
@@ -78,6 +99,7 @@ app.post('/api/transact', (req, res) => {
     res.json({ type: 'success', transaction });
 });
 
+
 app.get('/api/transaction-pool-map', (res, req) => {
     req.json(transactionPool.transactionMap)
 });
@@ -94,6 +116,20 @@ app.get('/api/wallet-info', (req, res) => {
         address,
         balance: Wallet.calculateBalance({ chain: blockchain.chain, address})
     }); 
+});
+
+app.get('/api/known-addresses', (req, res) => {
+    const addressMap = {}; 
+
+    for (let block of blockchain.chain) {
+        for (let transaction of block.data){
+            const recipient = Object.keys(transaction.outputMap); 
+
+            recipient.forEach(recipient => addressMap[recipient] = recipient);
+        }
+    }
+
+    res.json(Object.keys(addressMap));
 });
 
 app.get('*', (req, res) => {
@@ -152,7 +188,7 @@ if (isDevelopment) {
     });
 
 
-    for( let i=0; i < 10; i++) {
+    for( let i=0; i < 20; i++) {
     if(i%3 === 0){
         walletAction();
         walletFooAction();
