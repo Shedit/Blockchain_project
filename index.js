295c1de714587140e524cb52aa3e532e79df954c
Shedit <pyrt.91@gmail.com>
2020-01-22 10:15:06 -0500

    31, added non-sleep function.

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const PubSub = require('./app/pubsub');
 const TransactionPool = require('./wallet/transaction-pool');
 const Wallet = require('./wallet');
 const TransactionMiner = require('./app/transaction-miner');
+const http = require('http');
+
 
 const isDevelopment = process.env.ENV === 'development';
 
@@ -33,6 +35,12 @@ const path = require('path');
 
 
 
+
+if (!isDevelopment) {
+    setInterval(
+        () => {http.get("https://intense-wildwood-82940.herokuapp.com/")}, 
+        300)
+    };
 
 
 app.use(bodyParser.json());
