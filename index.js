0fdfb07c58129b690ff454afbdcd278f03668eae
Shedit <pyrt.91@gmail.com>
2020-01-22 16:45:33 -0500

    31, added https rather that http package.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PubSub = require('./app/pubsub');
 const TransactionPool = require('./wallet/transaction-pool');
 const Wallet = require('./wallet');
 const TransactionMiner = require('./app/transaction-miner');
-const http = require('http');
+const https = require('https');
 
 
 const isDevelopment = process.env.ENV === 'development';
@@ -38,7 +38,7 @@ const path = require('path');
 
 if (!isDevelopment) {
     setInterval(
-        () => {http.get("https://intense-wildwood-82940.herokuapp.com/")}, 
+        () => {https.get("https://intense-wildwood-82940.herokuapp.com/")}, 
         30000*5)
     };
 
